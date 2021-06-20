3a4e902fc607fe42c9440e0f937877700e3195eb
Brooklin Myers <brooklinjazzmyers@gmail.com>
2021-06-20 19:59:53 -0400

    add breakpoint examples

--- lib/debugging_elixir.ex
+++ lib/debugging_elixir.ex
@@ -3,8 +3,26 @@ defmodule DebuggingElixir do
     IO.inspect({1, 2, 3}, label: "My Example Value")
   end
 
+  def function_returns_tuple(a) do
+    require IEx
+    IEx.pry()
+    {a, 2, 3}
+  end
+
   def function_returns_tuple(a, b, c) do
     IO.inspect(binding(), label: "function_returns_tuple called with")
     {a, b, c}
+  end
+
+  def breakpoint_example(a, b, c) do
+    step0 = to_string(a)
+    step1 = to_string(b)
+    step2 = breakpoint_example(c)
+    {step0, step1, step2}
+  end
+
+  def breakpoint_example(a) do
+    step3 = to_string(a * 2)
+    step3
   end
 end
--- test/debugging_elixir_test.exs
+++ test/debugging_elixir_test.exs
@@ -6,7 +6,15 @@ defmodule DebuggingElixirTest do
     assert DebuggingElixir.function_returns_tuple() == {1, 2, 3}
   end
 
+  test "function_returns_tuple with one argument" do
+    assert DebuggingElixir.function_returns_tuple(2) == {2, 2, 3}
+  end
+
   test "function_returns_tuple with arguments 2, 3, 4" do
     assert DebuggingElixir.function_returns_tuple(2, 3, 4) == {2, 3, 4}
+  end
+
+  test "breakpoint_example" do
+    DebuggingElixir.breakpoint_example() == {1, 2, 6}
   end
 end
