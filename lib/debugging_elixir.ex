085794d5def094356c5bf6f54c57fbedfcc81cee
Brooklin Myers <brooklinjazzmyers@gmail.com>
2021-06-20 16:31:48 -0400

    demonstrate IO.inspect with label

--- lib/debugging_elixir.ex
+++ lib/debugging_elixir.ex
@@ -1,5 +1,5 @@
 defmodule DebuggingElixir do
   def function_returns_tuple do
-    IO.inspect({1, 2, 3})
+    IO.inspect({1, 2, 3}, label: "My Example Value")
   end
 end
--- test/debugging_elixir_test.exs
+++ test/debugging_elixir_test.exs
@@ -2,7 +2,7 @@ defmodule DebuggingElixirTest do
   use ExUnit.Case
   doctest DebuggingElixir
 
-  test "testing a tuple {1,2,3}" do
+  test "function_returns_tuple with no arguments" do
     assert DebuggingElixir.function_returns_tuple() == {1,2,3}
   end
 end
