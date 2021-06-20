8f7158e8c7f7a5f7b79d2bd8cb6e5423ba17966a
Brooklin Myers <brooklinjazzmyers@gmail.com>
2021-06-20 16:42:33 -0400

    demonstrate inspect binding

--- lib/debugging_elixir.ex
+++ lib/debugging_elixir.ex
@@ -2,4 +2,9 @@ defmodule DebuggingElixir do
   def function_returns_tuple do
     IO.inspect({1, 2, 3}, label: "My Example Value")
   end
+
+  def function_returns_tuple(a, b, c) do
+    IO.inspect(binding(), label: "function_returns_tuple called with")
+    {a, b, c}
+  end
 end
--- test/debugging_elixir_test.exs
+++ test/debugging_elixir_test.exs
@@ -3,6 +3,10 @@ defmodule DebuggingElixirTest do
   doctest DebuggingElixir
 
   test "function_returns_tuple with no arguments" do
-    assert DebuggingElixir.function_returns_tuple() == {1,2,3}
+    assert DebuggingElixir.function_returns_tuple() == {1, 2, 3}
+  end
+
+  test "function_returns_tuple with arguments 2, 3, 4" do
+    assert DebuggingElixir.function_returns_tuple(2, 3, 4) == {2, 3, 4}
   end
 end
